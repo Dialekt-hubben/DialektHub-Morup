f9ddc1783e77bf64670ff581220e8b6f50e7e1da
Josef Forkman <44498331+JosefForkman@users.noreply.github.com>
2026-01-09 15:10:51 +0100

    Add Drizzle ORM configuration for SQLite database

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  out: 'src/Drizzle/migrations',
+  schema: 'src/Drizzle/models/*.ts',
+  dialect: 'sqlite',
+  dbCredentials: {
+    url: process.env.DB_FILE_NAME!,
+  },
+});
