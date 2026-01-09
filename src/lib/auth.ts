1e795d32e2b92198b8e93a5077edb763669e5034
Josef Forkman <josef@forkman.dev>
2026-01-09 23:21:28 +0100

    Add authentication routes and client setup with better-auth integration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,15 @@
 import db from "@/Drizzle";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "sqlite",
-  })
+  }),
+  // "nextCookies" is always need to be lasted last in the plugins array
+  plugins: [nextCookies()],
+  emailAndPassword: {
+    enabled: true,
+  },
 });
