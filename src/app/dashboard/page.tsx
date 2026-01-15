2a8b4be3c70a8b6a2d5022bdb09fdd2fdceaf1b8
Josef Forkman <josef@forkman.dev>
2026-01-15 21:03:16 +0100

    ensure user is redirected to login if session is not available

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,21 @@
 import { signOut } from "@/actions/auth";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function DashboardPage() {
   const session = await auth.api.getSession({
     headers: await headers()
   });
+
+  // If no session, redirect to login
+  if (!session?.user) {
+    redirect("/Login");
+  }
   
   return (
     <main>
-      <h1>Hej {session?.user?.name}</h1>
+      <h1>Hej {session.user.name}</h1>
       <form action={signOut}>
         <button type="submit">Sign Out</button>
       </form>
