f01b83e3d3fb8c12cdaa420ba0f8dcbda35c8716
Kim Andersson <Kimpabooy@live.com>
2026-01-20 20:16:47 +0100

    table for words in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,34 @@
-import Image from "next/image";
+import db from "@/Drizzle";
 import styles from "./page.module.css";
+import { dialectWordTable } from "@/Drizzle/models/DialectWord"; 
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+export default async function Home() {
+  const data = await db.select().from(dialectWordTable);
+  const dialectWord = JSON.stringify(data);
+
+
+  let dataObject = data.map(item => ({
+    id: item.id,
+    word: item.word,
+    pronunciation: item.pronunciation,
+    phrase: item.phrase,
+    status: item.status,
+    userId: item.userId,
+    nationalWordId: item.nationalWordId,
+    soundFileId: item.soundFileId
+  }));
+  console.log("Filtered Data:", dataObject);
+  
+
+  return (    
+      <body>
+        <div>
+          <div className={styles["header"]}></div>
+          <div className={styles["db-list"]}>
+          {JSON.stringify(dataObject)}
+          </div>      
+          <div className={styles["footer"]}></div>      
         </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      </body>
   );
 }
