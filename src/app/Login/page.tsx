5eb9147cafea8899ac099f8acfdb437ca667f622
Josef Forkman <44498331+JosefForkman@users.noreply.github.com>
2026-01-30 09:15:46 +0100

    Login page layout and enhance form submission handling

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -6,11 +6,16 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
 import { signIn } from "@/actions/auth";
 import { useRouter } from "next/navigation";
+import style from './page.module.css';
 
 function Page() {
     const { push } = useRouter();
-    const { register, handleSubmit, formState: { errors }, setError } = useForm<Login>({
-        resolver: zodResolver(Login)
+    const { register, handleSubmit, formState: { errors, isSubmitting }, setError } = useForm<Login>({
+        resolver: zodResolver(Login),
+        defaultValues: {
+            email: "Josef@forkman.dev",
+            password: "Password1!"
+        }
     })
 
     const onSubmit = async (data: Login) => {
@@ -24,32 +29,37 @@ function Page() {
     
 
     return (
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-            <InputGroup
-                label="Email"
-                type="email"
-                id="email"
-                {...register("email")}
-                errorMessage={errors.email?.message}
-            />
-            <InputGroup
-                label="Password"
-                type="password"
-                id="password"
-                {...register("password")}
-                errorMessage={errors.password?.message}
-            />
-            {
-                errors.root?.message && (
-                    <p>{errors.root.message}</p>
-                )
-            }
-            <button type="submit" className="btn primary">Login</button>
-            <Link href="/" className="btn">Avbryt</Link>
-            </form>
-            <p>Don&apos;t have an account? <Link href="/Signup">Sign up here</Link></p>
-        </div>
+        <main className={style.main}>
+            <div className={style.LoginContainer}>
+                <div className={style.header}><h1>Login</h1></div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <InputGroup
+                        label="Email"
+                        type="email"
+                        id="email"
+                        {...register("email")}
+                        errorMessage={errors.email?.message}
+                    />
+                    <InputGroup
+                        label="Password"
+                        type="password"
+                        id="password"
+                        {...register("password")}
+                        errorMessage={errors.password?.message}
+                    />
+                    {
+                        errors.root?.message && (
+                            <p>{errors.root.message}</p>
+                        )
+                    }
+                    <div className={style.buttonGroup}>
+                        <button type="submit" className="btn primary" disabled={isSubmitting}>Login</button>
+                        <Link href="/" className="btn">Avbryt</Link>
+                    </div>
+                    <p>Don&apos;t have an account? <Link href="/Signup">Sign up here</Link></p>
+                </form>
+            </div>
+        </main>
     );
 }
 export default Page;
