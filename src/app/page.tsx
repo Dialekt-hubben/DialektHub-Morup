1a27e85d7e7c4987540463dda50c13abe4c5fd3c
Kim Andersson <Kimpabooy@live.com>
2026-02-01 17:39:10 +0100

    Refactor Home component for cleaner structure and improved data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,29 +4,26 @@ import styles from "./page.module.css";
 import Pagination from "../components/Pagination";
 import Table from "@/components/Table";
 
-const headers = [
-  "Ord",
-  "Ljudfil",
-  "Uttal",
-  "Användare",
-  "Svenska",
-];
-
-const PAGE_SIZE = 10; // Antal poster per sida
-
+// Main Page-component
 export default function Home() {
-  const [data, setData] = useState([]); // Data för den aktuella sidan
-  const [total, setTotal] = useState(0); // Totalt antal poster, används för att beräkna totala sidor
-  const [page, setPage] = useState(1); // börjar på sida 1
-  const totalPages = Math.ceil(total / PAGE_SIZE);
-
+  const [data, setData] = useState([]); // Data for current page
+  const [total, setTotal] = useState(0); // Total number of rows in the database
+  const [page, setPage] = useState(1); // Current page
+  const PAGE_SIZE = 10; // Amount of rows per page
+  const totalPages = Math.ceil(total / PAGE_SIZE); // Calculate total number of pages
+  
+  // Runs when "page" changes (or on first render)
   useEffect(() => {
     async function fetchData() {
-      const res = await fetch(`/api/dialectwords?page=${page}&pageSize=${PAGE_SIZE}`);
-      const result = await res.json();
-      setData(result.data || []);
-      setTotal(result.total || 0);
-      console.log("Fetched data:", result);
+    
+    // Fetch data from the API with page number and page size
+    const res = await fetch(`/api/dialectwords?page=${page}&pageSize=${PAGE_SIZE}`);
+    const result = await res.json();
+    console.log("Fetched data:", result); // (Debug) Show fetched data in the console
+    
+    // Save data and total variables from the API response
+    setData(result.data || []);
+    setTotal(result.total || 0);
     }
     fetchData();
   }, [page]);
@@ -34,13 +31,11 @@ export default function Home() {
   return (
     <main>
       <div className={styles.tableContainerWrapper}>
-        <div>
           <div className={styles.tableContainer}>
             <h2 className={styles.tableHeader}>Ordlista</h2>
             <Table data={data} />
+            <Pagination page={page} totalPages={totalPages} setPage={setPage} />
           </div>
-          <Pagination page={page} totalPages={totalPages} setPage={setPage} />
-        </div>
       </div>
     </main>
   );
