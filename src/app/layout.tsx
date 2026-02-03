03673c8c4e43b298530a7a05ec5249c8de2405c4
Josef Forkman <josef@forkman.dev>
2026-02-03 20:32:02 +0100

    Add Header component with styling and navigation links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
+import Header from "@/components/Header/Header";
 
 const robotoMono = Roboto_Mono({
   subsets: ["latin"],
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
+import style from "./Header.module.css";
 
 function Header() {
     return (
-        <header>
-            <Link href="/">
+        <header className={style.header}>
+            <Link href="/" className={style.logo}>
                 <Image src="/logo.png" alt="DialektHub Logo" width={50} height={50} />
                 <h1>DialektHub • Dialektlexikon</h1>
             </Link>
