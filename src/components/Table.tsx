5332adb91cf3018300c52cbb68cb1de6573bdf26
Josef Forkman <josef@forkman.dev>
2026-03-27 15:11:01 +0100

    Remove unused Uttal column from table header in Table component

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -22,7 +22,6 @@ export default function Table({ tableData }: TableProps) {
                     <tr>
                         <th className={styles.tableHeaderCell}>{"Dialekt"}</th>
                         <th className={styles.tableHeaderCell}>{"Ljudfil"}</th>
-                        <th className={styles.tableHeaderCell}>{"Uttal"}</th>
                         <th className={styles.tableHeaderCell}>{"Svenska"}</th>
                         <th className={styles.tableHeaderCell}>
                             {"Användare"}
