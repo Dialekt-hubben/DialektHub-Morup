4cea20493dc7e68e3c33256d30dca02ac5eba141
Josef Forkman <josef@forkman.dev>
2026-05-19 18:50:33 +0200

    Add AddWordForm2 component with audio recording functionality Co-authored-by: Kim Andersson <Kimpabooy@live.com>

---
+++ src/components/AddWordForm2.tsx
@@ -0,0 +1,117 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { InputGroup } from "./InputGroup";
+import { useForm } from "react-hook-form";
+import { addDialectWord } from "@/types/DialektFormValidation/dialectWord";
+import styles from "./AddWordForm.module.css";
+import Link from "next/link";
+import { CreateDialectWord } from "@/actions/dialectwords";
+import useAudio2 from "./Audio2";
+
+function AddWordForm2() {
+    const {
+        recordingSoundFile,
+        isRecording,
+        startRecording,
+        stopRecording,
+        playRecording,
+        pauseRecording,
+    } = useAudio2();
+    const {
+        handleSubmit,
+        register,
+        formState: { errors },
+        setError,
+        setValue,
+    } = useForm({
+        defaultValues: {
+            dialectWord: "test",
+            nationalWord: "test",
+        },
+        resolver: zodResolver(addDialectWord),
+    });
+
+    const onSubmit = async (data: addDialectWord) => {
+        try {
+            await CreateDialectWord(data);
+        } catch (error) {
+            if (error instanceof Error) {
+                setError("root", {
+                    message: error.message,
+                });
+            }
+        }
+    };
+
+    return (
+        <div>
+            <form className={styles.addWordForm} onSubmit={handleSubmit(onSubmit)}>
+                <h2>Lägg till nytt ord 2</h2>
+                <InputGroup
+                    label="Dialekt ord"
+                    placeholder="Skriv dialekt ordet här..."
+                    {...register("dialectWord")}
+                    errorMessage={errors.dialectWord?.message}
+                />
+                <InputGroup
+                    label="Svenskt ord"
+                    placeholder="Skriv det svenska ordet här..."
+                    {...register("nationalWord")}
+                    errorMessage={errors.nationalWord?.message}
+                />
+                <br />
+                <p>Ladda upp en ljudfil eller spela in direkt</p>
+                <InputGroup
+                    type="file"
+                    label="ljud fil"
+                    accept="audio/*"
+                    placeholder="Upload an audio file..."
+                    {...register("audioFile")}
+                    errorMessage={errors.audioFile?.message?.toString()}
+                />
+                {errors.root && <p>{errors.root.message}</p>}
+                <div>
+                    {!isRecording ? (
+                        <button
+                            type="button"
+                            className="btn primary"
+                            onClick={startRecording}>
+                            Spela in
+                        </button>
+                    ) : (
+                        <button
+                            type="button"
+                            className="btn primary"
+                            onClick={() => {
+                                stopRecording({ setValue });
+                                console.log({
+                                    recordingSoundFile: recordingSoundFile
+                                });
+                            }}>
+                            Stoppa inspelning
+                        </button>
+                    )}
+                    {recordingSoundFile && (
+                        <button
+                            type="button"
+                            className="btn primary"
+                            onClick={() => playRecording()}>
+                            Spela upp inspelning
+                        </button>
+                    )}
+                </div>
+                <button className="btn primary">Spara ord</button>
+                <Link href="/" className="btn primary">
+                    Avbryt
+                </Link>
+            </form>
+            {recordingSoundFile && (
+                <>
+                    <p>Inspelning pågår...</p>
+                    <pre>{recordingSoundFile[0].name}</pre>
+                </>
+            )}
+        </div>
+    );
+}
+export default AddWordForm2;
---
+++ src/components/Audio2.ts
@@ -0,0 +1,88 @@
+import { addDialectWord } from "@/types/DialektFormValidation/dialectWord";
+import { useRef, useState } from "react";
+import { UseFormSetValue } from "react-hook-form";
+
+function useAudio2() {
+    const [isRecording, setIsRecording] = useState(false);
+    const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+    const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+    const [recordingSoundFile, setRecordingSoundFile] = useState<FileList | null>(null);
+
+    async function startRecording() {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        const mediaRecorder = new MediaRecorder(stream, { mimeType: "audio/webm" });
+        mediaRecorderRef.current = mediaRecorder;
+
+        mediaRecorder.start();
+
+        setIsRecording(true);
+    }
+
+    async function stopRecording({
+        setValue,
+    }: {
+        setValue: UseFormSetValue<{
+            dialectWord: string;
+            nationalWord: string;
+            audioFile?: FileList | null | undefined;
+        }>;
+    }) {
+        if (!mediaRecorderRef.current) {
+            return;
+        }
+
+        // Stoppa inspelningen, vilket kommer att trigga ondataavailable-eventet där vi hanterar den inspelade ljudfilen
+        mediaRecorderRef.current.stop();
+
+        // När data är tillgänglig, skapa en fil och spara den i state
+        mediaRecorderRef.current.ondataavailable = (event: BlobEvent) => {
+            const audioBlob = event.data;
+            console.log({ type: audioBlob.type });
+
+            const audioFile = new File([audioBlob], "recording.webm", {
+                type: "audio/webm",
+            });
+
+            const fileList = new DataTransfer();
+            fileList.items.add(audioFile);
+
+            setRecordingSoundFile(fileList.files);
+            setValue("audioFile", fileList.files, { shouldValidate: false });
+        };
+
+        // Stäng av mikrofonen
+        mediaRecorderRef.current.stream.getTracks().forEach((track) => track.stop());
+        setIsRecording(false);
+    }
+
+    function playRecording() {
+        if (!recordingSoundFile) {
+            return;
+        }
+        const mediaAudio = new Audio();
+        const file = recordingSoundFile;
+        mediaAudio.src = URL.createObjectURL(file[0]);
+        setAudio(mediaAudio);
+        mediaAudio.play();
+    }
+
+    function pauseRecording() {
+        if (!recordingSoundFile) {
+            return;
+        }
+        if (audio) {
+            audio.pause();
+        }
+    }
+
+    return {
+        startRecording,
+        stopRecording,
+        isRecording,
+        recordingSoundFile,
+        playRecording,
+        pauseRecording,
+    };
+}
+
+export default useAudio2;
