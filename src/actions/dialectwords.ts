a9f52dd0619aa36fa5e53e6189840239453acf8c
Josef Forkman <josef@forkman.dev>
2026-05-19 17:01:08 +0200

    Prittifay file

--- src/actions/dialectwords.ts
+++ src/actions/dialectwords.ts
@@ -56,10 +56,7 @@ export async function GetAllDialectwords({ query, page, pageSize }: GetParams) {
             nationalWordTable,
             eq(dialectWordTable.nationalWordId, nationalWordTable.id),
         )
-        .leftJoin(
-            soundFileTable,
-            eq(dialectWordTable.soundFileId, soundFileTable.id),
-        )
+        .leftJoin(soundFileTable, eq(dialectWordTable.soundFileId, soundFileTable.id))
         .where(caseInsensitiveWordFilter)
         .limit(paginationSize)
         .offset(paginationOffset);
@@ -96,8 +93,7 @@ export async function CreateDialectWord(data: addDialectWord) {
 
     if (!fileParseResult.success) {
         throw new Error(
-            "Invalid input data: " +
-                JSON.stringify(fileParseResult.error.message),
+            "Invalid input data: " + JSON.stringify(fileParseResult.error.message),
         );
     }
 
@@ -107,6 +103,8 @@ export async function CreateDialectWord(data: addDialectWord) {
     const audioFileName = audioFile
         ? Date.now() + "-" + audioFile.name.toLowerCase()
         : null;
+
+    return;
     const existingDialectWord = await db
         .select({ word: dialectWordTable.word })
         .from(dialectWordTable)
@@ -124,14 +122,8 @@ export async function CreateDialectWord(data: addDialectWord) {
         .from(dialectWordTable)
         .where(
             and(
-                eq(
-                    dialectWordTable.word,
-                    existingDialectWord.at(0)?.word || "",
-                ),
-                eq(
-                    dialectWordTable.nationalWordId,
-                    existingNationalWord.at(0)?.id || 0,
-                ),
+                eq(dialectWordTable.word, existingDialectWord.at(0)?.word || ""),
+                eq(dialectWordTable.nationalWordId, existingNationalWord.at(0)?.id || 0),
             ),
         );
 
@@ -201,9 +193,7 @@ export async function UpdateDialectWord(data: updateDialectWord) {
     const parsedData = updateDialectWord.safeParse(data);
 
     if (!parsedData.success) {
-        throw new Error(
-            "Ogiltig data: id, dialectWord och nationalWord krävs.",
-        );
+        throw new Error("Ogiltig data: id, dialectWord och nationalWord krävs.");
     }
 
     // Normalize the input words to ensure consistency in the database.
@@ -221,9 +211,7 @@ export async function UpdateDialectWord(data: updateDialectWord) {
                 .limit(1);
 
             if (existingDialectWord.length === 0) {
-                throw new Error(
-                    "Kunde inte hitta dialektordet för det angivna id:t.",
-                );
+                throw new Error("Kunde inte hitta dialektordet för det angivna id:t.");
             }
 
             // Get the ID for the national word (nationalWord) from the nationalWordTable.
@@ -258,9 +246,7 @@ export async function UpdateDialectWord(data: updateDialectWord) {
                 .limit(1);
 
             if (duplicatePair.length > 0) {
-                throw new Error(
-                    "Det finns redan en identisk rad med dessa ord.",
-                );
+                throw new Error("Det finns redan en identisk rad med dessa ord.");
             }
 
             // If all checks pass, update the dialect word with the new values.
@@ -286,10 +272,7 @@ export async function UpdateDialectWord(data: updateDialectWord) {
     };
 }
 
-export async function UpdateDialectWordStatus(
-    dialectWordId: number,
-    newStatus: Status,
-) {
+export async function UpdateDialectWordStatus(dialectWordId: number, newStatus: Status) {
     const currentUser = await getAdminSession();
 
     if (!currentUser) {
@@ -297,10 +280,7 @@ export async function UpdateDialectWordStatus(
     }
 
     try {
-        if (
-            newStatus === Status.enum.approved ||
-            newStatus === Status.enum.pending
-        ) {
+        if (newStatus === Status.enum.approved || newStatus === Status.enum.pending) {
             await db
                 .update(dialectWordTable)
                 .set({ status: GetNumberFromStatus(newStatus) })
@@ -327,9 +307,7 @@ export async function UpdateDialectWordStatus(
                     Bucket: env.S3_BUCKET_NAME,
                     Key: wordToDelete.soundFileName,
                 } satisfies DeleteObjectCommandInput;
-                const deleteCommand = new DeleteObjectCommand(
-                    deleteCommandInput,
-                );
+                const deleteCommand = new DeleteObjectCommand(deleteCommandInput);
                 await s3Client.send(deleteCommand);
 
                 await db
@@ -338,9 +316,7 @@ export async function UpdateDialectWordStatus(
 
                 await db
                     .delete(soundFileTable)
-                    .where(
-                        eq(soundFileTable.fileName, wordToDelete.soundFileName),
-                    );
+                    .where(eq(soundFileTable.fileName, wordToDelete.soundFileName));
                 return;
             }
 
@@ -349,9 +325,7 @@ export async function UpdateDialectWordStatus(
                 .where(eq(dialectWordTable.id, dialectWordId));
         }
     } catch {
-        throw new Error(
-            "Ett fel uppstod vid uppdatering av dialektordets status.",
-        );
+        throw new Error("Ett fel uppstod vid uppdatering av dialektordets status.");
     }
 }
 
