4a5faa1fb5077ba0b66fb870ce8f41b59d67b824
Josef Forkman <josef@forkman.dev>
2026-09-07 15:00:17 +0200

    replace anchor tags with Link components in AdminSidebar for improved routing

--- src/components/Admin/AdminSidbar.tsx
+++ src/components/Admin/AdminSidbar.tsx
@@ -2,6 +2,7 @@
 
 import { usePathname } from "next/navigation";
 import styles from "../../app/adminView/layout.module.css";
+import Link from "next/link";
 
 export function AdminSidebar() {
     const pathname = usePathname();
@@ -12,20 +13,24 @@ export function AdminSidebar() {
                 <ul>
                     <li
                         className={`${styles.navItem} ${pathname === "/adminView" ? styles.active : ""}`}>
-                        <a href="/adminView">AdminVy</a>
+                        <Link href="/adminView">AdminVy</Link>
                     </li>
                     <li
                         className={`${styles.navItem} ${pathname === "/adminView/importExcel" ? styles.active : ""}`}>
-                        <a href="/adminView/importExcel">Importera Excelfil</a>
+                        <Link href="/adminView/importExcel">Importera Excelfil</Link>
                     </li>
                     <li
+                        className={`${styles.navItem} ${pathname === "/adminView/User" ? styles.active : ""}`}>
+                        <Link href="/adminView/User">Användare</Link>
+                    </li>
+                    {/* <li
                         className={`${styles.navItem} ${pathname === "/adminView/signup" ? styles.active : ""}`}>
                         <a href="/adminView/signup">Skapa ny användare</a>
                     </li>
                     <li
                         className={`${styles.navItem} ${pathname === "/adminView/userRoles" ? styles.active : ""}`}>
                         <a href="/adminView/userRoles">Ändra användarroller</a>
-                    </li>
+                    </li> */}
                 </ul>
             </nav>
         </aside>
