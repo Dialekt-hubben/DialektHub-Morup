9a10d2c98afe4ac43c0619cf7c78bb0dac7e6253
Josef Forkman <josef@forkman.dev>
2026-09-10 14:34:41 +0200

    Refactor user management table to use Table component

--- src/app/adminView/User/page.tsx
+++ src/app/adminView/User/page.tsx
@@ -1,48 +1,43 @@
 import { InputGroup } from "@/components/InputGroup";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserCog, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { Table, TableCell, TableRow } from "@/components/Table";
 
 function Page() {
     return (
         <main>
-            <form>
-                <InputGroup label="Sök" placeholder="Sök på email..." />
-                <button className="btn primary">Sök</button>
-            </form>
+            <Table
+                headerColumns={["Email", "Roll", "Status", "Skapad", "Åtgärder"]}
+                title="Användarna"
+                actions={
+                    <>
+                        <form>
+                            <InputGroup label="Sök" placeholder="Sök på email..." />
+                            <button className="btn primary">Sök</button>
+                        </form>
 
-            <button className="btn primary">Lägg till användare</button>
-
-            <table>
-                <thead>
-                    <tr>
-                        <th>Email</th>
-                        <th>Roll</th>
-                        <th>Status</th>
-                        <th>Skapad</th>
-                        <th>Åtgärder</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>
-                            <a href="mailto:user@example.com">user@example.com</a>
-                        </td>
-                        <td>Admin</td>
-                        <td>Active</td>
-                        <td>1 maj 2023</td>
-                        <td>
-                            <button>
-                                <span className="sr-only">Redigera</span>
-                                <FontAwesomeIcon icon={faUserCog} />
-                            </button>
-                            <button>
-                                <span className="sr-only">Ta bort</span>
-                                <FontAwesomeIcon icon={faTrashCan} />
-                            </button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+                        <button className="btn primary">Lägg till användare</button>
+                    </>
+                }>
+                <TableRow>
+                    <TableCell>
+                        <a href="mailto:user@example.com">user@example.com</a>
+                    </TableCell>
+                    <TableCell>Admin</TableCell>
+                    <TableCell>Active</TableCell>
+                    <TableCell>1 maj 2023</TableCell>
+                    <TableCell>
+                        <button>
+                            <span className="sr-only">Redigera</span>
+                            <FontAwesomeIcon icon={faUserCog} />
+                        </button>
+                        <button>
+                            <span className="sr-only">Ta bort</span>
+                            <FontAwesomeIcon icon={faTrashCan} />
+                        </button>
+                    </TableCell>
+                </TableRow>
+            </Table>
         </main>
     );
 }
