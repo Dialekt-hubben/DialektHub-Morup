06feef72145d20f962b4b2cbf87c61083cd752ae
Josef Forkman <josef@forkman.dev>
2026-09-13 22:30:09 +0200

    Add user retrieval and deletion functions with caching support

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,8 +1,13 @@
 "use server";
 
+import db from "@/Drizzle";
+import { user } from "@/Drizzle/models/auth-schema";
 import { auth } from "@/lib/auth";
 import { Login, Signup } from "@/types/auth";
+import { eq } from "drizzle-orm";
+import { cacheTag, revalidateTag } from "next/cache";
 import { headers } from "next/headers";
+import { cache } from "react";
 
 export async function signUp(data: Signup) {
     const parsedData = Signup.safeParse(data);
@@ -52,6 +57,36 @@ export async function signOut() {
     });
 }
 
+export const getUsers = cache(async function (limit: number, offset = 0) {
+    cacheTag("users");
+    try {
+        const response = await db.query.user.findMany({
+            columns: {
+                id: true,
+                email: true,
+                role: true,
+                createdAt: true,
+                updatedAt: true,
+            },
+            limit,
+            offset,
+        });
 
+        return response;
+    } catch (error) {
+        if (error instanceof Error) {
+            throw new Error(error.message);
+        }
+        throw new Error("Ett okänt fel inträffade vid hämtning av användare");
+    }
+});
 
+export async function deleteUser(userid: string) {
+    const deletedUser = await db.delete(user).where(eq(user.id, userid));
 
+    if (deletedUser[0].affectedRows === 0) {
+        throw new Error("Användaren kunde inte tas bort");
+    }
+
+    revalidateTag("users", { expire: 0 });
+}
